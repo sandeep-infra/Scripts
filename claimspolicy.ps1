a64ec7c22b157eb8d82fb15fa6a42c4833b9687e
sandeep-infra <85048854+sandeep-infra@users.noreply.github.com>
2025-04-22 11:23:08 +0530

    Update claimspolicy.ps1

--- claimspolicy.ps1
+++ claimspolicy.ps1
@@ -40,3 +40,39 @@ $policyDefinition = '{
 }'
 
 New-AzureADPolicy -Definition $policyDefinition -DisplayName "claims" -Type "ClaimsMappingPolicy"
+
+-------------------------------------------------------------------------------------------------------------------------------
+##WITH MGGraph module:
+
+
+
+# Connect to Microsoft Graph with required permissions
+Connect-MgGraph -Scopes "Policy.ReadWrite.ApplicationConfiguration", "Application.ReadWrite.All"
+
+# Create the claims mapping policy
+$policyParams = @{
+    "definition" = @('{"ClaimsMappingPolicy":{"Version":1,"IncludeBasicClaimSet":"true", "ClaimsSchema": [{"Source":"user","ID":"onPremisesSamAccountName","JwtClaimType":"onPremisesSamAccountName"}]}}')
+    "displayName" = "claims"
+}
+
+$policy = Invoke-MgGraphRequest -Method POST -Uri "/beta/policies/claimsMappingPolicies" -Body $policyParams
+
+# Assign the policy to the service principal
+$servicePrincipalId = "badfae04-4c39-42be-95f5-87e4d323aa1d"
+$policyId = $policy.id
+
+$refBody = @{
+    "@odata.id" = "https://graph.microsoft.com/beta/policies/claimsMappingPolicies/$policyId"
+}
+
+Invoke-MgGraphRequest -Method POST -Uri "/beta/servicePrincipals/$servicePrincipalId/claimsMappingPolicies/`$ref" -Body $refBody
+
+##For removing the policy:
+
+# Service Principal ID and Policy ID to remove
+$servicePrincipalId = "0fac8fd4-8ce3-481f-a38a-f796ee244e3c"
+$policyId = "12b86b96-b35d-4796-884c-ace8fee73a4b"  # Replace with actual Policy ID
+
+# Remove the policy assignment
+Invoke-MgGraphRequest -Method DELETE `
+    -Uri "/beta/servicePrincipals/$servicePrincipalId/claimsMappingPolicies/$policyId/`$ref"
